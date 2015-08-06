6e2413f55e9320128da1a034861724ded559051a
Braelan <bbraelan@gmail.com>
2015-08-06 14:26:57 -0400

    board sets pieces and renders

--- checkers/board.rb
+++ checkers/board.rb
@@ -8,8 +8,28 @@ class Board
   end
 
 
-  def render
+  def set
   end
+
+  def place_pieces(start, color)
+
+    orientation = start == 0 ? :down : :up
+    count = 0
+    grid.each_with_index do |row, row_idx|
+        row_idx = row_idx + start
+      row.each_with_index do |cell, col_idx|
+        if (col_idx + 2).even? && (row_idx +2).even?
+          self[[row_idx, col_idx]] = Piece.new([row_idx, col_idx], self, color, orientation)
+        elsif (col_idx+2).odd? && (row_idx).odd?
+          self[[row_idx, col_idx]] = Piece.new([row_idx, col_idx], self, color, orientation)
+        end
+      end
+      count = count +1
+      break if count == 3
+    end
+
+  end
+
 
   def make_grid
    Array.new(SIZE){Array.new(SIZE) }
@@ -45,4 +65,11 @@ class Board
     color = alt ? 0 : 1
     "   #{str}   ".colorize(:background => BOARD_COLORS[color])
   end
+
+  def move(start_pos, end_pos)
+    
+  end
+
+
+
 end
